bf8aadab3268f86196034047abc8cf1c822ac0ba
YATENDRA AWANA <yka6739@gmail.com>
2021-08-17 14:50:00 +0530

    Add project files.

---
+++ WebApplication15/Client/Shared/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication15.Client.Shared
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ WebApplication15/Shared/Info.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication15.Shared
+{
+    public class Info
+    {
+     
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
